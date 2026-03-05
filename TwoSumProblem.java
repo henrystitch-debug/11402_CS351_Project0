53eb61717ce31849458d4de408f7c5cf1ab47956
Olivia <olivia.meisohle@gmx.de>
2026-03-05 15:52:09 +0800

    Finished Two Sum Problems

--- TwoSumProblem.java
+++ TwoSumProblem.java
@@ -1,4 +1,4 @@
-class TwoSum {
+public class TwoSumProblem {
 static int[] twoSum(int[] nums, int target) {
 
     int no1 = 0;
@@ -10,11 +10,22 @@ static int[] twoSum(int[] nums, int target) {
                 no2 = j;
             }
         }
-
-
     }
     return new int[]{no1, no2};
 }
+
+static int[] twoSumHashtable(int[] nums, int target) {
+    java.util.HashMap<Integer, Integer> map = new java.util.HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[]{map.get(complement), i};
+        }
+        map.put(nums[i], i);
+    }
+    return new int[]{};
+}
+
 public static void main(String[] args) {
 
     int[] numbers = {1,2,3,4,5,6};
@@ -23,7 +34,11 @@ public static void main(String[] args) {
     int[] result = twoSum(numbers, target);
     System.out.println("Indices: " + result[0] + ", " + result[1]);
 
+    int[] result2 = twoSumHashtable(numbers, target);
+    System.out.println("Indices: " + result2[0] + ", " + result2[1]);
+
 
 
 }
+
 }
